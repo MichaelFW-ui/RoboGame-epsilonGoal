ec90eb5ed357c664cc197367068446dcd8eea827
zrc1564496890 <86159465+zrc1564496890@users.noreply.github.com>
2021-07-21 15:31:14 +0800

    Update trace.c

---
+++ User/src/trace.c
@@ -0,0 +1,13 @@
+#include ""
+
+
+struct Trace_Data{
+  uint8_t North[10];
+  uint8_t South[10];
+  uint8_t East[10];
+  uint8_t West[10];
+  uint8_t North_Caculate;
+  uint8_t South_Caculate;
+  uint8_t East_Caculate;
+  uint8_t West_Caculate;
+}Trace_Detect;
