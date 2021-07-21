15c6bfc363c70fa7cb836b373c5a648e55dc2420
zrc1564496890 <86159465+zrc1564496890@users.noreply.github.com>
2021-07-21 15:14:32 +0800

    Update motion.c

---
+++ User/src/motion.c
@@ -0,0 +1,54 @@
+#include ""
+
+//给个结构体对运动控制
+struct Motion_Control{
+  uint8_t Motion_Wheel;
+  uint8_t Motion_Left_Arm[3];
+  uint8_t Motion_Right_Arm{3];
+  uint8_t Motion_Pushbeam;
+  uint8_t Motion_Frictiongear;
+};
+
+//前后左右移动以及停下
+void Motion_GoNorth(void){
+}
+
+void Motion_GoSouth(void){
+}
+
+void Motion_GoEast(void){
+}
+
+void Motion_GoWest(void){
+}
+
+void Motion_Stay(void){
+}
+
+//左右机械臂伸展
+void Motion_LeftArmSpread(void){
+}
+
+void Motion_RightArmSpread(void){
+}
+
+//左右机械臂先收紧爪子然后转向竖直
+void Motion_LeftArmLiftUp(void){
+}
+
+void Motion_RightArmLiftUp(void){
+}
+
+//左右机械臂转向球盒，放开球
+void Motion_LeftDropBall(void){
+}
+
+void Motion_RightDropBall(void){
+}
+
+//开始扔球
+void Motion_StartThrowing(void){
+}
+
+void Motion_EndThrowing(void){
+}
