ee9443ef20daa1a446507cd7f3a48f8212590f68
zrc1564496890 <86159465+zrc1564496890@users.noreply.github.com>
2021-07-28 16:44:58 +0800

    Update ranging.c

--- User/src/ranging.c
+++ User/src/ranging.c
@@ -1,15 +1,16 @@
 #include "vl53l0x_api.h"
+#include "ranging.h"
 
 
 VL53L0X_RangingMeasurementData_t RangingData;
-VL53L0X_Dev_t  vl53l0x_c; // center module
+VL53L0X_Dev_t  vl53l0x_c; // 这个好像是主体
 VL53L0X_DEV    Dev = &vl53l0x_c;
 Dev->I2cHandle = &hi2c1;
 Dev->I2cDevAddr = 0x52;
 
 
 void Ranging_Init(void){
-  HAL_GPIO_WritePin(TOF_XSHUT_GPIO_Port, TOF_XSHUT_Pin, GPIO_PIN_RESET); // Disable XSHUT
+  HAL_GPIO_WritePin(TOF_XSHUT_GPIO_Port, TOF_XSHUT_Pin, GPIO_PIN_RESET); // 失能XSHUT
   HAL_Delay(20);
   HAL_GPIO_WritePin(TOF_XSHUT_GPIO_Port, TOF_XSHUT_Pin, GPIO_PIN_SET); // 使能XSHUT
   HAL_Delay(20);
@@ -20,7 +21,7 @@ void Ranging_Init(void){
   VL53L0X_PerformRefSpadManagement(Dev, &refSpadCount, &isApertureSpads);
   VL53L0X_SetDeviceMode(Dev, VL53L0X_DEVICEMODE_SINGLE_RANGING);
 
-  // Enable/Disable Sigma and Signal check
+  // 初始化为单次测试，改mode可以改成连续测
   VL53L0X_SetLimitCheckEnable(Dev, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
   VL53L0X_SetLimitCheckEnable(Dev, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
   VL53L0X_SetLimitCheckValue(Dev, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, (FixPoint1616_t)(0.1*65536));
